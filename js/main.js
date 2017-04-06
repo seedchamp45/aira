2da21a516f92e648d9e241262d27a5708fa00078
chinoros <chinoros.x@gmail.com>
2017-04-06 18:24:40 +0700

    add

---
+++ js/main.js
@@ -0,0 +1,41 @@
+ 
+$(function(){
+ var idOld="";
+// hide bg
+ if(idOld == ""){
+    $(".page").hide();
+ }
+    // chang menu bar
+   $("a").click(function() {
+        var name = this.name;
+        var cls = this.className;
+         
+        //remove class
+        $("#"+ idOld +'_txt').removeClass('menu_action');
+         $("#"+ idOld +'_img').attr("src",'images/menu/'+ idOld +".png");
+        
+         //add class
+        idOld = this.id;
+       
+        $("#"+ idOld +'_img').attr("src",'images/menu/'+ idOld +"_action.png");
+        $("#"+ idOld +'_txt').addClass('menu_action');
+        load_page();
+    });
+    
+    function load_page(){
+        $(".page").show();
+       
+        $("#content_page").load("aira_network.html", function(responseTxt, statusTxt, jqXHR){
+                if(statusTxt == "success"){
+                    alert("success!");
+                }
+                if(statusTxt == "error"){
+                    alert("Error: " + jqXHR.status + " " + jqXHR.statusText);
+                }
+        });
+    }
+});
+
+
+
+
